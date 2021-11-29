a732d6aa73afa495c96cc0fef09de7fefbf3dc84
Yagoo23 <yagocg7@gmail.com>
2021-11-29 18:59:30 +0100

    Nuevo menú posible

--- Proyecto_Juego/main.py
+++ Proyecto_Juego/main.py
@@ -1,7 +1,13 @@
 import pygame
-import sys
 import constantes
 import random
+from juego import Juego
+
+j=Juego()
+while j.running:
+    j.playing=True
+    j.loop_juego()
+
 
 pygame.init()
 """ 
@@ -122,49 +128,48 @@ for p in range(Max_Platforms):
     el bucle principal del juego
     """
 
+# def main_menu():
+#     selected = "start"
 
-def main_menu():
+while True:
+    reloj.tick(FPS)
+    jumpy.movimiento()
+    # hace aparecer el fondo
+    screen.blit(fondo, (0, 0))
+    # dibuja
+    platform_grupo.draw(screen)
+    jumpy.dibujar()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    #         quit()
+    #     if event.type == pygame.KEYDOWN:
+    #         if event.key == pygame.K_UP:
+    #             selected = "start"
+    #         elif event.key == pygame.K_DOWN:
+    #             selected = "quit"
+    #         if event.key == pygame.K_SPACE:
+    #             if selected == "start":
+    #                 run = True
+    #             if selected == "quit":
+    #                 quit()
+    # screen.fill(constantes.blue)
+    # title = text_format("Saltos", constantes.font, 90, constantes.yellow)
+    # if selected == "start":
+    #     text_start = text_format("START", constantes.font, 75, constantes.white)
+    # else:
+    #     text_start = text_format("START", constantes.font, 75, constantes.black)
+    # if selected == "quit":
+    #     text_quit = text_format("QUIT", constantes.font, 75, constantes.white)
+    # else:
+    #     text_quit = text_format("QUIT", constantes.font, 75, constantes.black)
+    # title_rect = title.get_rect()
+    # start_rect = text_start.get_rect()
+    # quit_rect = text_quit.get_rect()
+    # screen.blit(title, (constantes.SCREEN_WIDTH / 2 - (title_rect[2] / 2), 80))
+    # screen.blit(text_start, (constantes.SCREEN_WIDTH / 2 - (start_rect[2] / 2), 300))
+    # screen.blit(text_quit, (constantes.SCREEN_WIDTH / 2 - (quit_rect[2] / 2), 360))
+    pygame.display.update()
 
-    selected="start"
-    run=True
-    while run:
-        reloj.tick(FPS)
-        jumpy.movimiento()
-        # hace aparecer el fondo
-        screen.blit(fondo, (0, 0))
-        # dibuja
-        platform_grupo.draw(screen)
-        jumpy.dibujar()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    selected = "start"
-                elif event.key == pygame.K_DOWN:
-                    selected = "quit"
-                if event.key == pygame.K_SPACE:
-                    if selected == "start":
-                        run=True
-                    if selected == "quit":
-                        quit()
-        screen.fill(constantes.blue)
-        title = text_format("Saltos", constantes.font, 90, constantes.yellow)
-        if selected == "start":
-            text_start = text_format("START", constantes.font, 75, constantes.white)
-        else:
-            text_start = text_format("START", constantes.font, 75, constantes.black)
-        if selected == "quit":
-            text_quit = text_format("QUIT", constantes.font, 75, constantes.white)
-        else:
-            text_quit = text_format("QUIT", constantes.font, 75, constantes.black)
-        title_rect = title.get_rect()
-        start_rect = text_start.get_rect()
-        quit_rect = text_quit.get_rect()
-        screen.blit(title, (constantes.SCREEN_WIDTH / 2 - (title_rect[2] / 2), 80))
-        screen.blit(text_start, (constantes.SCREEN_WIDTH / 2 - (start_rect[2] / 2), 300))
-        screen.blit(text_quit, (constantes.SCREEN_WIDTH / 2 - (quit_rect[2] / 2), 360))
-        pygame.display.update()
-main_menu()
+# main_menu()
 pygame.quit()
